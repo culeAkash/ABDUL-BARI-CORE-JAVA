d60d3d9ba5ecb79a954e2e0fd66434cf11149031
Akash <akashjais929@gmail.com>
2022-01-17 12:28:52 +0530

    Multithreading
    Monitoring and ATM class

--- Multithreading/ThreadClass.java
+++ Multithreading/ThreadClass.java
@@ -7,7 +7,7 @@ class MyRun implements Runnable{
 class MyThreadNew extends Thread{
      public MyThreadNew(String name){
          super(name);
-        //  setPriority(MAX_PRIORITY);
+         setPriority(MAX_PRIORITY);
      }//To give the name to the Thread class
 
     public void run(){
@@ -15,14 +15,14 @@ class MyThreadNew extends Thread{
         while(true){
             System.out.println(count++ +" MyThread");
             //it is really fast we can delay it by using sleep method
-            // try{
-            //     Thread.sleep(1000);//This will put the thread to sleep for 1000 milisecond and then run for ome time and then again for 1000 miliseconds
-            //     //It throws Interrupted Exception that must be catched
-            // }
-            // catch(InterruptedException e){
-            //     System.out.println(e);
-            //     //As we interrpted the thread from main thread during sleep it catched the Interrupt Exception, we called it one time only
-            // }
+            try{
+                 Thread.sleep(1000);//This will put the thread to sleep for 1000 milisecond and then run for ome time and then again for 1000 miliseconds
+                 //It throws Interrupted Exception that must be catched
+             }
+             catch(InterruptedException e){
+                 System.out.println(e);
+                 //As we interrpted the thread from main thread during sleep it catched the Interrupt Exception, we called it one time only
+             }
             
         }
     }
@@ -54,8 +54,8 @@ public class ThreadClass {
 
        //the main thread has limit functionalities in it but the other thread has infinite loop, so main thread gets executed before it, but doesn't goes to termination as it waits for the other thread to end
 
-        // Thread mainThread= Thread.currentThread();
-        // mainThread.join();//Now though the other thread is daemon thread it will also wait for that thread
+        Thread mainThread= Thread.currentThread();//To get the current thread
+         mainThread.join();//Now though the other thread is daemon thread it will also wait for that thread
 
         int count=1;
         while(true){
